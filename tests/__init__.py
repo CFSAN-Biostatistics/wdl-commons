0de73d8104c6e43fd1a04c935a63de8414d96886
Justin Payne <crashfrog@gmail.com>
2024-10-31 13:46:57 +0700

    fallback to submodule

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,17 @@
 import pytest
 import tempfile
 import requests
+from pathlib import Path
 
 def use_file(partial_path, name="file"):
     def file():
         __name__ = name
         with tempfile.NamedTemporaryFile() as f:
-            f.write(requests.get(f"https://raw.githubusercontent.com/CFSAN-Biostatistics/data-commons/{partial_path}").content)
-            yield f.name
+            try:
+                f.write(requests.get(f"https://raw.githubusercontent.com/CFSAN-Biostatistics/data-commons/{partial_path}").content)
+                yield f.name
+            except requests.HTTPError:
+                yield Path(__file__).parent / partial_path
     def decorator(func):
         return pytest.mark.usefixtures(file)(func)
     return decorator
